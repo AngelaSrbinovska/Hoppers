5cc9bffbb4d76e7623c4d8274a7109f3f64e8698
AngelaSrbinovska <106864451+AngelaSrbinovska@users.noreply.github.com>
2022-08-29 11:53:01 -0400

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module BFSPuzzleSolver {
-    requires transitive javafx.controls;
-    exports puzzles.common;
-    exports puzzles.common.solver;
-    exports puzzles.hoppers.gui;
-    exports puzzles.hoppers.model;
-    exports puzzles.jam.gui;
-    exports puzzles.jam.model;
-}
